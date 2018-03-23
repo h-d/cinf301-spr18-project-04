d3c6c8fefdc93d3d13fec4b872de3116c9aa1315
hdevoe <hdevoe@stetson.edu>
2018-03-23 13:11:42 -0400

    updating tweets

--- index.php
+++ index.php
@@ -1,6 +1,37 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
+require_once('vendor/j7mbo/twitter-api-php/TwitterAPIExchange.php');
+require_once('config.inc.php');
+
+if(!isset($_SESSION['settings']))
+{
+    $_SESSION['settings'] = array(
+        'oauth_access_token' => OAUTH_ACCESS_TOKEN,
+        'oauth_access_token_secret' => OAUTH_ACCESS_TOKEN_SECRET,
+        'consumer_key' => CONSUMER_KEY,
+        'consumer_secret' => CONSUMER_SECRET);
+
+}
+
+
+if (!isset($_SESSION['twitter']))
+{
+    $_SESSION['twitter'] = new TwitterAPIExchange($_SESSION['settings']);
+}
+
+/*
+$url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+$getfield = '?screen_name=ClickHole';
+$requestMethod = 'GET';
+
+$response = $_SESSION['twitter']->setGetfield($getfield)
+    ->buildOauth($url, $requestMethod)
+    ->performRequest();
+
+echo $response;
+*/
+
 if (isset($_GET['controller']) && isset($_GET['action'])) {
     $controller = $_GET['controller'];
     $action = $_GET['action'];
